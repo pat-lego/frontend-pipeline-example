ec50f3ef4e5485be5cf0c231a173a3a6205f3ffb
Patrique Legault <patrique.legault@gmail.com>
2023-06-23 12:14:43 -0400

    Removing functions

--- src/js/helloworld.js
+++ src/js/helloworld.js
@@ -1,13 +1,1 @@
-function docReady(fn) {
-    // see if DOM is already available
-    if (document.readyState === "complete" || document.readyState === "interactive") {
-        // call on next available tick
-        setTimeout(fn, 1);
-    } else {
-        document.addEventListener("DOMContentLoaded", fn);
-    }
-}
-
-docReady(function() {
-    console.log("Hello World!!! - I was called from the front end pipeline");
-});
+console.log("Hello World!!! - I was called from the front end pipeline");
